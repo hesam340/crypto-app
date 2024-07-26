7528348bfc8ed5d6d05d0ffd212a4626063489d5
hesam khaki <hesamkhaki340@gmail.com>
2024-07-26 20:56:13 +0330

    search component is completed & it's styles are added

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
+import { RotatingLines } from "react-loader-spinner";
+
 import { searchCoin } from "../../services/cryptoApi";
+
+import styles from "./Search.module.css";
 
 function Search({ currency, setCurrency }) {
   const [text, setText] = useState("");
   const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-    if (!text) return;
+    setCoins([]);
+    if (!text) {
+      setIsLoading(false);
+      return;
+    }
+
     const search = async () => {
       try {
         const res = await fetch(searchCoin(text), {
@@ -16,6 +26,7 @@ function Search({ currency, setCurrency }) {
         const json = await res.json();
         if (json.coins) {
           setCoins(json.coins);
+          setIsLoading(false);
         } else {
           alert.status.error_message;
         }
@@ -26,13 +37,14 @@ function Search({ currency, setCurrency }) {
         }
       }
     };
+    setIsLoading(true);
     search();
 
     return () => controller.abort();
   }, [text]);
 
   return (
-    <div>
+    <div className={styles.searchBox}>
       <input
         type="text"
         placeholder="Search ..."
@@ -44,6 +56,26 @@ function Search({ currency, setCurrency }) {
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
+      {(!!coins.length || isLoading) && (
+        <div className={styles.searchResult}>
+          {isLoading && (
+            <RotatingLines
+              width="50px"
+              height="50px"
+              strokeColor="#5a5aff"
+              strokeWidth="3"
+            />
+          )}
+          <ul>
+            {coins.map((coin) => (
+              <li key={coin.id}>
+                <img src={coin.thumb} alt={coins.name} />
+                <p>{coin.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
