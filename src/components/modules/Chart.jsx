e743a41ea4a0f65c39dedb82589136c6902f29e1
hesam khaki <hesamkhaki340@gmail.com>
2024-07-27 20:49:01 +0330

    chart component is completed & chart is created by recharts library

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,15 +1,99 @@
 import styles from "./Chart.module.css";
 
-import 
+import { convertData } from "../../helper/convertData.js";
 
-function Chart({ chart, setChart }) {
-  console.log(chart);
+import { useState } from "react";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+function Chart({ chart, setChart, currency }) {
+  const [type, setType] = useState("prices");
+
+  const typeHandler = (event) => {
+    if (event.target.tagName === "BUTTON") {
+      const type = event.target.innerText.toLowerCase().replace(" ", "_");
+      setType(type);
+    }
+  };
   return (
     <div className={styles.container}>
-      <span className={styles.cross} onClick={()=>setChart(null)}>X</span>
-      <div className={styles.chart}></div>
+      <span className={styles.cross} onClick={() => setChart(null)}>
+        X
+      </span>
+      <div className={styles.chart}>
+        <div className={styles.name}>
+          <img src={chart.coin.image} alt={chart.coin.name} />
+          <p>{chart.coin.name}</p>
+        </div>
+        <div className={styles.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
+        <div className={styles.types} onClick={(event) => typeHandler(event)}>
+          <button className={type === "prices" ? styles.selected : null}>
+            Prices
+          </button>
+          <button className={type === "market_caps" ? styles.selected : null}>
+            Market Caps
+          </button>
+          <button className={type === "total_volumes" ? styles.selected : null}>
+            Total Volumes
+          </button>
+        </div>
+        <div className={styles.details}>
+          <div>
+            <p>Price :</p>
+            <span>
+              {currency === "usd" ? "$ " : currency === "eur" ? "€ " : "¥ "}
+              {chart.coin.current_price.toLocaleString()}
+            </span>
+          </div>
+          <div>
+            <p>ATH :</p>
+            <span>
+              {currency === "usd" ? "$ " : currency === "eur" ? "€ " : "¥ "}
+              {chart.coin.ath.toLocaleString()}
+            </span>
+          </div>
+          <div>
+            <p>Market Cap :</p>
+            <span>
+              {currency === "usd" ? "$ " : currency === "eur" ? "€ " : "¥ "}
+              {chart.coin.market_cap.toLocaleString()}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Chart;
+
+const ChartComponent = ({ data, type }) => {
+  console.log(data);
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={500} height={500} data={data}>
+        <CartesianGrid strokeDasharray="3 3" stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" hide />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3674ff"
+          strokeWidth="2px"
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -70,11 +70,17 @@ const TableRow = ({ coin, currency, setChart }) => {
         </div>
       </td>
       <td>{name}</td>
-      <td>${current_price.toLocaleString()}</td>
+      <td>
+        {currency === "usd" ? "$ " : currency === "eur" ? "€ " : "¥ "}
+        {current_price.toLocaleString()}
+      </td>
       <td className={price_change > 0 ? styles.success : styles.error}>
         {price_change.toFixed(2)}%
       </td>
-      <td>${total_volume.toLocaleString()}</td>
+      <td>
+        {currency === "usd" ? "$ " : currency === "eur" ? "€ " : "¥ "}
+        {total_volume.toLocaleString()}
+      </td>
       <td>
         <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
       </td>
